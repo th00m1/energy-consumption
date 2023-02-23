cb79008056f4546ac53c912b53845e417f3da1fb
Thomas Hamon <thomashamon@hotmail.fr>
2023-02-23 23:51:12 +0100

    Init

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-scss-boilerplate'`, () => {
+  it(`should have as title 'angular-folders-by-feature'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-scss-boilerplate');
+    expect(app.title).toEqual('angular-folders-by-feature');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-scss-boilerplate app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-folders-by-feature app is running!');
   });
 });
